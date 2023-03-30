899e28831e7792fbcae8db03af7fe3a0a94ef606
GalaKrond <74582518+GalaKrond-jkr0404@users.noreply.github.com>
2023-03-30 11:14:53 +0900

    500ms / 505ms flash for uno

---
+++ sketch_mar16a.ino
@@ -0,0 +1,30 @@
+unsigned long curr_time = 0;
+unsigned long pre_time;
+int cnt = 0;
+int cnt1 = 0;
+int Time_cnt = 0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop() {
+  curr_time = millis();
+  if (pre_time != curr_time){ // 1ms마다 조건 충족
+    pre_time = curr_time;
+    if(cnt++>=500){ // 500ms마다 조건 충족
+      cnt = 0;
+      Time_cnt++;
+      Serial.println(Time_cnt);
+      digitalWrite(13,digitalRead(13)^1);
+    }
+        if(cnt++>=505){ // 505ms마다 조건 충족
+      cnt1 = 0;
+      Time_cnt++;
+      Serial.println(Time_cnt);
+      digitalWrite(12,digitalRead(12)^1);
+
+    }
+  }
+}
